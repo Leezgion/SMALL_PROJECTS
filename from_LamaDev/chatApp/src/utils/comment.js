06593796a1cd525346617a39f3a8f8c7995875f2
Leezgion <1402510774@qq.com>
2024-01-11 09:53:32 +0800

    optimize:时间戳转换函数

--- from_LamaDev/chatApp/src/utils/comment.js
+++ from_LamaDev/chatApp/src/utils/comment.js
@@ -1,19 +1,19 @@
 export const timesFun = function (timesData) {
   //如果时间格式是正确的，那下面这一步转化时间格式就可以不用了
-  var dateBegin = timesData; //将-转化为/，使用new Date
-  var dateEnd = Date.now(); //获取当前时间
-  var dateDiff = dateEnd - dateBegin; //时间差的毫秒数
-  var yearDiff = Math.floor(dateDiff / (24 * 3600 * 1000 * 365)); //相差年数
-  var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000)); //计算出相差天数
-  var leave1 = dateDiff % (24 * 3600 * 1000); //计算天数后剩余的毫秒数
-  var hours = Math.floor(leave1 / (3600 * 1000)); //计算出小时数
+  let dateBegin = timesData; //将-转化为/，使用new Date
+  let dateEnd = Date.now(); //获取当前时间
+  let dateDiff = dateEnd - dateBegin; //时间差的毫秒数
+  let yearDiff = Math.floor(dateDiff / (24 * 3600 * 1000 * 365)); //相差年数
+  let dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000)); //计算出相差天数
+  let subDayMilliseconds = dateDiff % (24 * 3600 * 1000); //计算天数后剩余的毫秒数
+  let hours = Math.floor(subDayMilliseconds / (3600 * 1000)); //计算出小时数
   //计算相差分钟数
-  var leave2 = leave1 % (3600 * 1000); //计算小时数后剩余的毫秒数
-  var minutes = Math.floor(leave2 / (60 * 1000)); //计算相差分钟数
+  let subHourMilliseconds = subDayMilliseconds % (3600 * 1000); //计算小时数后剩余的毫秒数
+  let minutes = Math.floor(subHourMilliseconds / (60 * 1000)); //计算相差分钟数
   //计算相差秒数
-  var leave3 = leave2 % (60 * 1000); //计算分钟数后剩余的毫秒数
-  var seconds = Math.round(leave3 / 1000);
-  var timesString = "";
+  let subMinuteMilliseconds = subHourMilliseconds % (60 * 1000); //计算分钟数后剩余的毫秒数
+  let seconds = Math.round(subMinuteMilliseconds / 1000);
+  let timesString = "";
   if (yearDiff !== 0) {
     timesString = yearDiff + "年前";
   } else if (yearDiff === 0 && dayDiff !== 0) {
